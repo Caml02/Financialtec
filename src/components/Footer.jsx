e132957416939f3eb7a9fe379da6824449a9b510
Cristian Morales <cristianmorales3006@gmail.com>
2023-03-28 10:22:38 -0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-
 function Footer() {
     return (
         <footer className="footer-expand-lg bg-body-tertiary" data-bs-theme="dark">
-            <div className="container-fluid" >
+            <div className="container-fluid text-center py-3">
                 <div className="row">
                     <div className="col-md-6">
-                        <p className="footer-text"> Todos los derechos reservados © {new Date().getFullYear()}</p>
+                        <p className="footer-text"> © {new Date().getFullYear()}. Todos los derechos reservados a FinancialTec</p>
                     </div>
-                    <div className="col-md-6 text-end">
-                    <p className="footer-text">Desarrollado por CristianMorales | 
-                        <a href="https://www.linkedin.com/in/cristian-morales-87311b18a/" target="_blank" rel="noreferrer">
-                            <FaLinkedin />
-                        </a>
-                        <a href="https://github.com/Caml02/Financialtec" target="_blank" rel="noreferrer">
-                            <FaGithub />
-                        </a>
-                    </p>
+                    <div className="col-md-6">
+                        <p className="footer-text">Desarrollado por CristianMorales | 
+                            <a href="https://www.linkedin.com/in/cristian-morales-87311b18a/" target="_blank" rel="noreferrer">
+                                <FaLinkedin />
+                            </a>
+                            <a href="https://github.com/Caml02/Financialtec" target="_blank" rel="noreferrer">
+                                <FaGithub />
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
